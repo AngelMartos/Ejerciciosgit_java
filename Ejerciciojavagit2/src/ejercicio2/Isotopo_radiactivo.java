714b4e49fb5f8fd22376507e695b2062274accd9
AngelMartos <93266855+AngelMartos@users.noreply.github.com>
2021-12-09 16:23:25 +0100

    Add files via upload

---
+++ Ejerciciojavagit2/src/ejercicio2/Isotopo_radiactivo.java
@@ -0,0 +1,27 @@
+package ejercicio2;
+import  java.util.Scanner;
+
+public class Isotopo_radiactivo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		float porcentaje;
+		int i;
+		long tiempo;
+		double mitad = 0,5;
+		int resultado;
+		
+		
+		System.out.println("El isotopo esta al 100%");
+		System.out.println("Que porcentaje quieres meter");
+		porcentaje=scanner.nextFloat();
+		while(System.currentTimeMillis()-tiempo)<26*60*1000)) 
+			
+		
+
+		System.out.println("Tardaria en descomponerse hasta ese porcentaje"+tiempo);
+		
+	}
+
+}
